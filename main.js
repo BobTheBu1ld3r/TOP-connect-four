41cf9c4e5039985ea3191b773f7c8c2353983d81
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-06-15 13:44:10 +0100

    Create GameBoard class

--- main.js
+++ main.js
@@ -2,4 +2,25 @@ function Cell() {
   let value = null;
   const addToken = (playerToken) => (value = playerToken);
   const getToken = () => value;
+  return { addToken, getToken };
+}
+
+function GameBoard() {
+  const board = [];
+
+  for (let i = 0; i < 6; i++) {
+    board[i] = [];
+    for (let j = 0; j < 7; j++) {
+      board[i][j] = Cell();
+    }
+  }
+
+  const getBoard = () => board;
+
+  const printBoard = () => {
+    //helper function for printing the board
+    board.forEach((row) => {
+      console.log(row.map((cell) => cell.getToken()));
+    });
+  };
 }
